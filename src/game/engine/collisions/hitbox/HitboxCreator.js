137e19ad141aa261c37393aabd19334288ecce45
dehlya <dehlya.herbelin@hes-so.ch>
2023-06-02 15:46:46 +0200

    hotbox detailed + clean code a lil bit

--- src/game/engine/collisions/hitbox/HitboxCreator.js
+++ src/game/engine/collisions/hitbox/HitboxCreator.js
@@ -4,7 +4,6 @@ export class HitboxCreator {
     }
     defineLimits(imgPath){
         let img = new Image();
-        //img.id = 'imgId';
         img.src = imgPath;
         img.onload = () => {
             let canvas = document.createElement('canvas');
@@ -19,43 +18,23 @@ export class HitboxCreator {
             
             let hitbox = this.createHitboxFromImageData(imageData, transparencyData);
             console.log(hitbox);
-        //     document.body.appendChild(img);
-        //     let myImg = document.querySelector("#imgId");
-        //     let realWidth = myImg.naturalWidth;
-        //     let realHeight = myImg.naturalHeight;
-        //     let imgCanvas = document.createElement('canvas');
-        //     let imgCtx = imgCanvas.getContext('2d', {willReadFrequently: true});
-        //     imgCtx.drawImage(img, 0, 0);
-            
-        //     console.log( realHeight, realWidth);
-        //     for (let i = 0; i < realHeight; i++) {
-        //         for (let j = 0; j < realWidth; j++) {
-        //             let pixel = imgCtx.getImageData(i, j, 1, 1);
-        //             let data = pixel.data;
-        //             if(data[0] != 0 || data[1]!=0 || data[2]!=0){
-        //                 pixel.data[0] = 255;
-        //                 pixel.data[1] = 255;
-        //                 pixel.data[2] = 255;
-        //                 imgCtx.putImageData(pixel, i, j);
-        //                 console.log(i, j)
-        //             }
-        //         }
-        //     }
-            
-        //     document.body.appendChild(imgCanvas);
+            this.drawHitbox(ctx, hitbox);
+            document.body.appendChild(canvas);
 
         }
     }
     getTransparencyData(imageData){
-        let transparencyData ;
-        transparencyData = new Uint8ClampedArray(imageData.data.length);
-        console.log(imageData.data);
-        for (let i = 0; i < imageData.data.length; i++) {
+        const transparencyData = new Uint8ClampedArray(imageData.width * imageData.height);
+
+        for (let i = 0; i < imageData.width * imageData.height; i++) {
+        transparencyData[i] = imageData.data[i * 4 + 3]; // Assuming alpha channel is at index 3
         }
+
         return transparencyData;
     }
     createAll(){
-        this.defineLimits('../../../../../ressources/site/sketch/ennemy.png');
+        this.defineLimits('../../../../../ressources/game/ennemies/ennemy1_facing_back.png');
+        this.defineLimits('../../../../../ressources/game/character/attack/attack_down_2.png');
     }
 
     // Recursive function to find hitbox boundaries
@@ -65,61 +44,64 @@ export class HitboxCreator {
             x < 0 ||
             y < 0 ||
             x >= imageData.width ||
-            y >= imageData.height ||
-            transparencyData[(x + y * imageData.width) * 4 + 3] === 0
+            y >= imageData.height
         ) {
             return;
         }
     
         // Update hitbox boundaries
         if (x < hitbox.x) {
-            hitbox.width += hitbox.x - x;
             hitbox.x = x;
         }
         if (y < hitbox.y) {
-            hitbox.height += hitbox.y - y;
             hitbox.y = y;
         }
         if (x > hitbox.x + hitbox.width) {
-            hitbox.width = x - hitbox.x + 1;
+            hitbox.width = x - hitbox.x;
         }
         if (y > hitbox.y + hitbox.height) {
-            hitbox.height = y - hitbox.y + 1
+            hitbox.height = y - hitbox.y;
         }
     
         // Recursive calls for adjacent pixels
-        this.findHitboxBounds(imageData, transparencyData, x - 1, y, hitbox);
-        this.findHitboxBounds(imageData, transparencyData, x + 1, y, hitbox);
-        this.findHitboxBounds(imageData, transparencyData, x, y - 1, hitbox);
-        this.findHitboxBounds(imageData, transparencyData, x, y + 1, hitbox);
+        this.findHitboxBounds(imageData, transparencyData, x - 1, y, hitbox);  // Left
+        this.findHitboxBounds(imageData, transparencyData, x + 1, y, hitbox);  // Right
+        this.findHitboxBounds(imageData, transparencyData, x, y - 1, hitbox);  // Up
+        this.findHitboxBounds(imageData, transparencyData, x, y + 1, hitbox);  // Down
     }
 
     // Function to create hitbox from image data and transparency information
     createHitboxFromImageData(imageData, transparencyData) {
-        let hitbox = {
-            x: imageData.width,   // Initialize with maximum possible values
-            y: imageData.height,  // Initialize with maximum possible values
-            width: 0,             // Initialize with minimum possible value
-            height: 0             // Initialize with minimum possible value
-        };
-        // function for non recusive method
+        
+        let hitbox = { vertices: [] };
+
         for (let y = 0; y < imageData.height; y++) {
             for (let x = 0; x < imageData.width; x++) {
-              const pixelIndex = (x + y * imageData.width) * 4;
-              const isTransparent = transparencyData[pixelIndex] === 0;
-        
-              if (!isTransparent) {
-                hitbox.x = Math.min(hitbox.x, x);
-                hitbox.y = Math.min(hitbox.y, y);
-                hitbox.width = Math.max(hitbox.width, x - hitbox.x + 1);
-                hitbox.height = Math.max(hitbox.height, y - hitbox.y + 1);
-              }
+                const index = y * imageData.width + x;
+                const alpha = transparencyData[index];
+
+                if (alpha > 0) {
+                    hitbox.vertices.push({ x, y });
+                }
             }
         }
 
-        // Start recursive function from top-left corner of the image
-        //this.findHitboxBounds(imageData, transparencyData, 0, 0, hitbox);
-
         return hitbox;
+
+
+    }
+    drawHitbox(ctx, hitbox) {
+        ctx.strokeStyle = 'red';
+        ctx.lineWidth = 2;
+
+        ctx.beginPath();
+        ctx.moveTo(hitbox.vertices[0].x, hitbox.vertices[0].y);
+
+        for (let i = 1; i < hitbox.vertices.length; i++) {
+        ctx.lineTo(hitbox.vertices[i].x, hitbox.vertices[i].y);
+        }
+
+        ctx.closePath();
+        ctx.stroke();
     }
 }
