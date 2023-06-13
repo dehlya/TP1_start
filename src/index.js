559ef3c1ac6e7a91a68bf1267736e8b1efe5127f
M-E <marie-esther.mabillard@students.hevs.ch>
2023-06-13 23:07:35 +0200

    adding default redirect in DOM

--- src/index.js
+++ src/index.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Description from './site/Description'
 import Flow from './site/Flow'
 import Logbook from './site/Logbook'
 import Mockup from './site/Mockup'
 import Sketch from './site/Sketch'
 import Game from './site/game'
-import Index from './site/Index'
+import App from './site/App'
 
 
-ReactDOM.render(
-<BrowserRouter>
-    <Routes>
-        <Route path="index" element={<Index />} />
+function RedirectToIndex() {
+    return <Navigate to="/index" replace />;
+  }
+  
+  ReactDOM.render(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RedirectToIndex />} /> {/* Redirect empty path to index */}
+        <Route path="index" element={<App />} />
         <Route path="description" element={<Description />} />
         <Route path="flow" element={<Flow />} />
         <Route path="logbook" element={<Logbook />} />
         <Route path="mockup" element={<Mockup />} />
         <Route path="sketch" element={<Sketch />} />
         <Route path="game" element={<Game />} />
-    </Routes>
-</BrowserRouter>,
-document.getElementById('root')
-);
-
+      </Routes>
+    </BrowserRouter>,
+    document.getElementById('root')
+  );
