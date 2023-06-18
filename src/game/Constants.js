093a36c4ca5c5cf9e6cffdb8d98d09a493bed44d
dehlya <dehlya.herbelin@hes-so.ch>
2023-06-18 19:43:08 +0200

    refresh rate back to 60

--- src/game/Constants.js
+++ src/game/Constants.js
@@ -1,2 +1,2 @@
-export const REFRESH_RATE =  120; // 120 FPS si better than 60 hahaha 
+export const REFRESH_RATE =  60; // 120 FPS is better than 60 but browser doesnt support hahaha 
 export const SCREEN_RATIO = 16/9;
